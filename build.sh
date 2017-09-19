a7b41f5023cb1ed032303a7b7cb812325c5e2740
nichcream <c.potato23@gmail.com>
2017-09-19 16:41:09 +0800

    land: Fix zipping mistake

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
+dir=${PWD}
 # copy system files
 cp -Rf system output
+cd output
 
 # test existence of boot.img
 status_zip=1
-test -f output/boot.img && echo "packing zip file..." || status_zip=0
+test -f boot.img && echo "packing zip file..." || status_zip=0
 if [ ${status_zip} -eq 0 ]; then echo "boot.img not present, aborting..." && exit
 fi
 
 # ZIP IT!
-rm -rf output/.*
-zip -r ROM.zip output/*
+rm -rf .*
+zip -r ../ROM.zip *
 
+cd ${PWD}
 echo "Successfully exported flashable zip as ROM.zip!"
