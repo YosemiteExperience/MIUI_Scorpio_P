e6483d0a82d96596aca9d893a648212ec7a73324
nichcream <c.potato23@gmail.com>
2017-09-19 16:01:40 +0800

    land: Add build_kernel.sh
    Don't judge!

---
+++ build_bootimg.sh
@@ -0,0 +1,13 @@
+dir=${PWD}
+
+echo "clearing output/boot.img"
+test -f output/boot.img && rm -f output/boot.img || echo "nothing to clear"
+
+cd kernel
+echo "clearing out kernel/boot.img"
+test -f boot.img && rm -f boot.img || echo "nothing to clear"
+./mkboot boot.img boot_extracted
+cd ${dir}
+
+echo "copying new boot.img to output dir"
+mv kernel/boot.img output/boot.img
