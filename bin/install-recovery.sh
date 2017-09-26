967f747ad7ea8f4ba8fad8faffd5832622ee51f4
nichcream <c.potato23@gmail.com>
2017-09-26 17:40:49 +0000

    MIUI: Sync up with Dev 7.9.21

--- bin/install-recovery.sh
+++ bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:30747982:719f20e615dce9d64d82eca1704e6b269f32a959; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:29345098:ec97307324cecbe6fc8480f1e0073738099a421a EMMC:/dev/block/bootdevice/by-name/recovery 719f20e615dce9d64d82eca1704e6b269f32a959 30747982 ec97307324cecbe6fc8480f1e0073738099a421a:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:30754126:f7a384e4c98244cfd57bce62d59338d28761f81f; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:29349194:6a0ae50036f1215f471657e59f31dbc96d0b23fb EMMC:/dev/block/bootdevice/by-name/recovery f7a384e4c98244cfd57bce62d59338d28761f81f 30754126 6a0ae50036f1215f471657e59f31dbc96d0b23fb:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
